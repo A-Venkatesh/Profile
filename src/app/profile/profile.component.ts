d2d5692c9aba582cd47b529fd5e726b60fd60e59
A-Venkatesh <venkatesha@live.in>
2024-08-20 16:33:21 -0400

    Docker and image load fix

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -18,7 +18,7 @@ import { DockModule } from 'primeng/dock';
 import { ButtonModule } from 'primeng/button';
 import { MenuItem } from 'primeng/api';
 import { TimelineModule } from 'primeng/timeline';
-import { faUser, faBriefcase, faGraduationCap, faAward } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faBriefcase, faGraduationCap, faAward, faMicrochip } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -87,9 +87,31 @@ export class ProfileComponent implements OnInit {
   };
 
   skillImages: { [key: string]: string } = {
-    Java: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
-    'Spring Boot': 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png'
+    'ad': 'assets/logos/ad.svg',
+    'amazon-web-services': 'assets/logos/amazon-web-services.svg',
+    'analytics': 'assets/logos/analytics.svg',
+    'angular': 'assets/logos/angular.svg',
+    'api-manager': 'assets/logos/api-manager.svg',
+    'apigee': 'assets/logos/apigee.svg',
+    'azure': 'assets/logos/azure.svg',
+    'bigquery': 'assets/logos/bigquery.svg',
+    'cloud-data-fusion': 'assets/logos/cloud-data-fusion.svg',
+    'cloud-functions': 'assets/logos/cloud-functions.svg',
+    'cloud-logging': 'assets/logos/cloud-logging.svg',
+    'cloud-pubsub': 'assets/logos/cloud-pub-sub.svg', // assuming ':' is replaced with '-'
+    'cloud-storage': 'assets/logos/cloud-storage.svg',
+    'cosmos-db': 'assets/logos/cosmos-db.svg',
+    'google-cloud-platform': 'assets/logos/google-cloud-platform.svg',
+    'java': 'assets/logos/java.svg',
+    'junit': 'assets/logos/junit.svg',
+    'key-vault': 'assets/logos/key-vault.svg',
+    'kotlin': 'assets/logos/kotlin.svg',
+    'node.js': 'assets/logos/node.js.svg', // assuming '.' is used in the file name
+    'spring-boot': 'assets/logos/spring-boot.svg',
+    'sql': 'assets/logos/sql.svg',
+    'typescript': 'assets/logos/typescript.svg'
   };
+  
 
   defaultSkillImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyAWmAe5UBdinr66fGFYymYvFCenoK9pizhg&s';
 
@@ -119,22 +141,26 @@ export class ProfileComponent implements OnInit {
         target: 'about'
       },
       {
+        label: 'Skills',
+        faIcon: faMicrochip,  // FontAwesome icon
+        target: 'skills'
+      },
+      {
         label: 'Experience',
         faIcon: faBriefcase,  // FontAwesome icon
         target: 'experience'
       },
       {
-        label: 'Skills',
+        label: 'Education',
         faIcon: faGraduationCap,  // FontAwesome icon
-        target: 'skills'
+        target: 'education'
       },
       {
         label: 'Award',
         faIcon: faAward,  // FontAwesome icon
-        target: 'award'
+        target: 'awards'
       }
     ];
-    
   }
 
   getSkillCategories() {
@@ -148,20 +174,8 @@ export class ProfileComponent implements OnInit {
   getSkillImage(skill: string): string {
     const basePath = 'assets/logos/';
     const skillNameVariations = skill.toLowerCase().replace(/\s+/g, '-');
-    const imagePath = `${basePath}${skillNameVariations}.svg`;
 
-    try {
-      const req = new XMLHttpRequest();
-      req.open('HEAD', imagePath, false);
-      req.send();
-      if (req.status === 200) {
-        return imagePath;
-      }
-    } catch (e) {
-      // Continue to fallback if error occurs
-    }
-
-    return this.skillImages[skill] || this.defaultSkillImage;
+    return this.skillImages[skillNameVariations] || this.defaultSkillImage;
   }
 
   jumpToSection(section: string | null) {
